85f90290c46bf7de36557f293f719d8a121a2f84
Yassin Siouda <yassin@siouda.com>
2022-12-29 21:21:45 +0100

    FEAT wip product component

---
+++ front/src/component/products/Products.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import {getItems} from '$lib/api';
+    export let materialId: number;
+    let products = getItems("Products");
+
+    let product : object;
+
+    console.log(materialId);
+
+    async function filterProducts(products, materialId) {
+        const getProduct = await products.forEach((product) => {
+            product.materials.forEach((material) => {
+                if(material == materialId) {
+                    return product;
+                }
+            }); 
+        });
+        product = getProduct;
+    }
+
+    filterProducts(products, materialId);
+
+    console.log(product);
+
+
+</script>
+<section>
+    <h1 class="text-4xl text-white uppercase font-black mb-10">Produits liés</h1>
+</section>
